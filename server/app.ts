bec788f09e38ddf477daef2e7aa0fee5833d2d3f
Colin Kennedy <colin.kennedy@teamaol.com>
2017-03-09 12:14:11 +0000

    Adding One smart search component

--- server/app.ts
+++ server/app.ts
@@ -10,6 +10,7 @@ class SearchAndRescueApp {
     app.use(bodyParser.json());
     app.use('/', express.static('ui'));
     app.use('/vendor/', express.static('node_modules'));
+    app.use('/app/', express.static('app'));
     app.use('/api/', new AppRouter().getRouter());
     app.use(morgan('dev'));
 
--- ui/app.js
+++ ui/app.js
@@ -2,7 +2,8 @@
 
 // Declare app level module which depends on views, and components
 angular.module('searchAndRescueApp', [
-    'ngRoute'
+    'ngRoute',
+    'one.components'
   ])
   .config(applicationBootstrap);
 
