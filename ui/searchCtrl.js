60d3f93d58d1d6f182d340c1c85c68853b32a3ea
Colin Kennedy <colin.kennedy@teamaol.com>
2017-03-09 22:22:05 +0000

    Added charts

--- ui/searchCtrl.js
+++ ui/searchCtrl.js
@@ -6,11 +6,11 @@ angular.module('searchAndRescueApp')
 searchController.$inject = ['$scope', 'searchService'];
 
 function searchController($scope, searchService) {
-                           
+
   let vm = this;
   $scope.searchQuery = '';
-  
-  vm.onSearchBoxKeyPress = function($event, searchQuery) {
+
+  vm.onSearchBoxKeyPress = function ($event, searchQuery) {
     if ($event.keyCode === 13) { // ENTER
       console.log("Enter key pressed");
       vm.executeSearch(searchQuery);
@@ -66,5 +66,155 @@ function searchController($scope, searchService) {
         }
       ]
     });
-  }
+  };
+
+  let pieChartNode = $("#pieChart");
+  let myPieChart = new Chart(pieChartNode, {
+    type: 'pie',
+    data: {
+      labels: [
+        "Desktop",
+        "Mobile"
+      ],
+      datasets: [
+        {
+          data: [300, 700],
+          backgroundColor: [
+            "#FF6384",
+            "#36A2EB",
+            "#FFCE56"
+          ],
+          hoverBackgroundColor: [
+            "#FF6384",
+            "#36A2EB",
+            "#FFCE56"
+          ]
+        }]
+    },
+    options: {
+      animation: {
+        animateScale: true
+      },
+      responsive: true,
+      title: {
+        display: true,
+        text: 'Inventory Availability'
+      }
+    }
+  });
+
+  let lineChartNode = $("#lineChart");
+  let lineChart = new Chart(lineChartNode, {
+    type: 'line',
+    data: {
+      labels: ["January", "February", "March", "April", "May", "June", "July"],
+      datasets: [
+        {
+          label: "Revenue",
+          fill: false,
+          lineTension: 0.1,
+          backgroundColor: "rgba(75,192,192,0.4)",
+          borderColor: "rgba(75,192,192,1)",
+          borderCapStyle: 'butt',
+          borderDash: [],
+          borderDashOffset: 0.0,
+          borderJoinStyle: 'miter',
+          pointBorderColor: "rgba(75,192,192,1)",
+          pointBackgroundColor: "#fff",
+          pointBorderWidth: 1,
+          pointHoverRadius: 5,
+          pointHoverBackgroundColor: "rgba(75,192,192,1)",
+          pointHoverBorderColor: "rgba(220,220,220,1)",
+          pointHoverBorderWidth: 2,
+          pointRadius: 1,
+          pointHitRadius: 10,
+          data: [20, 59, 65, 78, 72, 85, 90],
+          spanGaps: false,
+        }
+      ]
+    },
+    options: {
+      responsive: true,
+      title: {
+        display: true,
+        text: 'Revenue Trend'
+      }
+    }
+  });
+
+  let barChartNode = $("#barChart");
+  let barChart = new Chart(barChartNode, {
+    type: 'horizontalBar',
+    data: {
+      labels: ["GM Amnet", "Best Buy", "Progressive", "Statefarm"],
+      datasets: [{
+        label: 'Campaign Revenue $M',
+        data: [12, 19, 3, 5],
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)'
+        ],
+        borderColor: [
+          'rgba(255,99,132,1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)'
+        ],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero:true
+          }
+        }]
+      },
+      responsive: true,
+      title: {
+        display: true,
+        text: 'Recent Customers'
+      }
+    }
+  });
+
+  let radarChartNode = $("#radarChart");
+  let radarChart = new Chart(radarChartNode, {
+    type: 'radar',
+    data: {
+      labels: ["Click", "Conversion", "Video Complete", "Impression", "Something Else", "Other", "Thing"],
+      datasets: [
+        {
+          label: "Mobile",
+          backgroundColor: "rgba(179,181,198,0.2)",
+          borderColor: "rgba(179,181,198,1)",
+          pointBackgroundColor: "rgba(179,181,198,1)",
+          pointBorderColor: "#fff",
+          pointHoverBackgroundColor: "#fff",
+          pointHoverBorderColor: "rgba(179,181,198,1)",
+          data: [65, 59, 90, 81, 56, 55, 40]
+        },
+        {
+          label: "Desktop",
+          backgroundColor: "rgba(255,99,132,0.2)",
+          borderColor: "rgba(255,99,132,1)",
+          pointBackgroundColor: "rgba(255,99,132,1)",
+          pointBorderColor: "#fff",
+          pointHoverBackgroundColor: "#fff",
+          pointHoverBorderColor: "rgba(255,99,132,1)",
+          data: [28, 48, 40, 19, 96, 27, 100]
+        }
+      ]
+    },
+    options: {
+      responsive: true,
+      title: {
+        display: true,
+        text: 'Estimate Cost Per'
+      }
+    }
+  });
 }
