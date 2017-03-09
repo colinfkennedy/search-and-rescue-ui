23d6cec4ed67504eb0286b7e2e0147a250ef6f16
Colin Kennedy <colin.kennedy@teamaol.com>
2017-03-09 10:00:12 +0000

    Removing dist

--- dist/server/controllers/searchAndRescueController.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class SearchAndRescueController {
-    search(req, res) {
-        console.log("Request: ", req);
-    }
-    ;
-}
-exports.SearchAndRescueController = SearchAndRescueController;
