319a6c4fd21ec8c855d113497922d48117c1fc12
Colin Kennedy <colin.kennedy@teamaol.com>
2017-03-10 11:27:36 +0000

    Using one css

--- ui/app.js
+++ ui/app.js
@@ -2,8 +2,7 @@
 
 // Declare app level module which depends on views, and components
 angular.module('searchAndRescueApp', [
-    'ngRoute',
-    'one.components'
+    'ngRoute'
   ])
   .config(applicationBootstrap);
 
